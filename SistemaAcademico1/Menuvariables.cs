0b97ec561e3c4c40ad1ec1f4f6ab405c45b56956
sveraa6-sys <sveraa6@uteq.edu.ec>
2026-06-28 19:05:36 -0500

    nueva ventana

---
+++ SistemaAcademico1/Menuvariables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SistemaAcademico1
+{
+    public partial class Menuvariables : Form
+    {
+        public Menuvariables()
+        {
+            InitializeComponent();
+        }
+    }
+}
