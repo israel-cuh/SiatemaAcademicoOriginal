ec1f4e75dccb57c4bc08482956f8ee5b7b87b41d
israel-cuh <morisrael438@gmail.com>
2026-07-18 16:18:32 -0500

    Agregar logica al formulario MenuTemaFor

--- SistemaAcademico1/MenuTemaFor.cs
+++ SistemaAcademico1/MenuTemaFor.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
+using System;
 using System.Windows.Forms;
 
 namespace SistemaAcademico1
@@ -13,6 +8,47 @@ namespace SistemaAcademico1
         public MenuTemaFor()
         {
             InitializeComponent();
+
+            if (System.ComponentModel.LicenseManager.UsageMode != System.ComponentModel.LicenseUsageMode.Designtime)
+            {
+                MenuLateralHelper.AgregarMenu(this, "Ciclos");
+                ControlesVentanaHelper.Agregar(this, cerrarAplicacion: true);
+            }
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            txtSalida.Clear();
+
+            for (int i = 1; i <= 5; i++)
+                txtSalida.AppendText(i + Environment.NewLine);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtSalida.Clear();
+        }
+
+        private void btnAnterior_Click(object sender, EventArgs e)
+        {
+            MenuTemaCiclo ventana = new MenuTemaCiclo();
+            ventana.Show();
+            Hide();
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "El formulario de Ciclo Do While todavía está pendiente.",
+                "CodeNova",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        private void btnPracticar_Click(object sender, EventArgs e)
+        {
+            using MinijuegoOrdenarCodigo juego = new MinijuegoOrdenarCodigo();
+            juego.ShowDialog(this);
         }
     }
 }
