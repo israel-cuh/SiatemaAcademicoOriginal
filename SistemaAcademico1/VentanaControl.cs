7f746c67e41115ebf7eab1e17f6531949059483c
israel-cuh <morisrael438@gmail.com>
2026-07-18 15:43:30 -0500

    Quitar maximizar y corregir propiedades de diseñador

--- SistemaAcademico1/VentanaControl.cs
+++ SistemaAcademico1/VentanaControl.cs
@@ -1,11 +1,17 @@
 using System;
+using System.ComponentModel;
 using System.Windows.Forms;
 
 namespace SistemaAcademico1
 {
     public partial class VentanaControl : UserControl
     {
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public Form? FormularioActual { get; set; }
+
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public bool CerrarAplicacion { get; set; } = true;
 
         public VentanaControl()
@@ -18,25 +24,6 @@ namespace SistemaAcademico1
             Form? formulario = FormularioActual ?? FindForm();
             if (formulario != null)
                 formulario.WindowState = FormWindowState.Minimized;
-        }
-
-        private void btnMaximizar_Click(object sender, EventArgs e)
-        {
-            Form? formulario = FormularioActual ?? FindForm();
-            if (formulario == null)
-                return;
-
-            if (formulario.WindowState == FormWindowState.Maximized)
-            {
-                formulario.WindowState = FormWindowState.Normal;
-                btnMaximizar.Text = "□";
-            }
-            else
-            {
-                formulario.MinimumSize = System.Drawing.Size.Empty;
-                formulario.WindowState = FormWindowState.Maximized;
-                btnMaximizar.Text = "❐";
-            }
         }
 
         private void btnCerrar_Click(object sender, EventArgs e)
