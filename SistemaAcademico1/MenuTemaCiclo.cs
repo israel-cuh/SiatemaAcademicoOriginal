655ef76f7972cacff06b9fdf588094977431f0bc
israel-cuh <morisrael438@gmail.com>
2026-07-18 17:43:12 -0500

    Abrir Ciclo Do While en formulario separado

--- SistemaAcademico1/MenuTemaCiclo.cs
+++ SistemaAcademico1/MenuTemaCiclo.cs
@@ -36,7 +36,7 @@ namespace SistemaAcademico1
 
             btnWhile.Click += (_, _) => MostrarWhile();
             btnFor.Click += (_, _) => AbrirCicloFor();
-            btnDoWhile.Click += (_, _) => MostrarDoWhile();
+            btnDoWhile.Click += (_, _) => AbrirCicloDoWhile();
             btnAnidados.Click += (_, _) => MostrarAnidados();
             btnjuego.Click += btnjuego_Click;
 
@@ -152,6 +152,13 @@ namespace SistemaAcademico1
         private void AbrirCicloFor()
         {
             MenuTemaFor ventana = new MenuTemaFor();
+            ventana.Show();
+            Hide();
+        }
+
+        private void AbrirCicloDoWhile()
+        {
+            MenuTemaDoWhile ventana = new MenuTemaDoWhile();
             ventana.Show();
             Hide();
         }
