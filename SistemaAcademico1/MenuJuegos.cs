519a9dbab30b7c4fa7332314a636490d60457f63
israel-cuh <morisrael438@gmail.com>
2026-07-19 11:57:22 -0500

    Agregar menu de juegos

---
+++ SistemaAcademico1/MenuJuegos.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Windows.Forms;
+
+namespace SistemaAcademico1
+{
+    public partial class MenuJuegos : Form
+    {
+        public MenuJuegos()
+        {
+            InitializeComponent();
+
+            if (System.ComponentModel.LicenseManager.UsageMode != System.ComponentModel.LicenseUsageMode.Designtime)
+            {
+                MenuLateralHelper.AgregarMenu(this, "Juego");
+                ControlesVentanaHelper.Agregar(this, cerrarAplicacion: true);
+            }
+        }
+
+        private void btnOrdenarCodigo_Click(object sender, EventArgs e)
+        {
+            using MinijuegoOrdenarCodigo juego = new MinijuegoOrdenarCodigo();
+            juego.ShowDialog(this);
+        }
+
+        private void btnJuego2_Click(object sender, EventArgs e)
+        {
+            MostrarPendiente("Juego 2");
+        }
+
+        private void btnJuego3_Click(object sender, EventArgs e)
+        {
+            MostrarPendiente("Juego 3");
+        }
+
+        private void btnJuego4_Click(object sender, EventArgs e)
+        {
+            MostrarPendiente("Juego 4");
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            MenuEstructura estructura = new MenuEstructura();
+            estructura.Show();
+            Hide();
+        }
+
+        private void MostrarPendiente(string nombreJuego)
+        {
+            MessageBox.Show(
+                $"{nombreJuego} todavía está pendiente. Aquí podrá ir el juego de otro integrante del equipo.",
+                "CodeNova - Juegos",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+}
