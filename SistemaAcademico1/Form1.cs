62ec0ef955efbd627f0fe333615b74a09b241055
israel-cuh <morisrael438@gmail.com>
2026-07-19 10:45:18 -0500

    Abrir dashboard despues del login

--- SistemaAcademico1/Form1.cs
+++ SistemaAcademico1/Form1.cs
@@ -91,13 +91,13 @@ namespace SistemaAcademico1
 
             if (usuario == "estudiante" && clave == "1234")
             {
-                MenuEstructura frm = new MenuEstructura("Estudiante");
+                DashboardPrincipal frm = new DashboardPrincipal("Estudiante");
                 frm.Show();
                 Hide();
             }
             else if (usuario == "docente" && clave == "1234")
             {
-                MenuEstructura frm = new MenuEstructura("Docente");
+                DashboardPrincipal frm = new DashboardPrincipal("Docente");
                 frm.Show();
                 Hide();
             }
